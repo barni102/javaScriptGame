20b61019e78e47d1555b9e21cf2a27426b26aee5
barni102 <kovacs102@gmail.com>
2020-12-29 15:17:56 +0100

    Ball created

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Paddle from "/src/paddle.js";
 import InputHandler from "/src/input.js";
+import Ball from "/src/ball.js";
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
@@ -8,6 +9,7 @@ const gameWidth = 1280;
 const gameHeight = 720;
 
 let paddle = new Paddle(gameWidth, gameHeight);
+let ball = new Ball();
 
 new InputHandler(paddle);
 
@@ -20,6 +22,7 @@ function gameLoop(timestmap) {
     ctx.clearRect(0, 0, 1280, 720);
     paddle.update(deltaTime);
     paddle.draw(ctx);
+    ball.draw(ctx);
 
     requestAnimationFrame(gameLoop);
 }
