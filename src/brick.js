88f1936ebb28c8d93872a2652e18f5472e4c7bd0
barni102 <kovacs102@gmail.com>
2021-01-07 15:17:30 +0100

    collisionDetections added and refactored ball collison

--- src/brick.js
+++ src/brick.js
@@ -1,3 +1,5 @@
+import { detectCollision } from "./collisionDetections.js";
+
 export default class Brick {
     constructor(game, position) {
         this.image = document.getElementById("img_brick");
@@ -8,9 +10,16 @@ export default class Brick {
 
         this.width = 128;
         this.height = 24;
+
+        this.markedForDeletion = false;
     }
 
-    update() {}
+    update() {
+        if (detectCollision(this.game.ball, this)) {
+            this.game.ball.speed.y = -this.game.ball.speed.y;
+            this.markedForDeletion = true;
+        }
+    }
 
     draw(ctx) {
         ctx.drawImage(this.image, this.position.x, this.position.y, this.width, this.height);
